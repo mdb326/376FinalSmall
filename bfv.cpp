dad3a675853ac29b64054a653f90ddf3307b83f4
Matthew Bergin <mdb326@io.cse.lehigh.edu>
2026-04-28 17:38:24 -0400

    Fix some naming stuff

--- bfv.cpp
+++ bfv.cpp
@@ -5,49 +5,38 @@
 
 using namespace std;
 
-using Poly = vector<int64_t>;
-
-// Parameters
 const int N = 16;
 const int64_t q = 1 << 15; // 32768
-const int64_t t = 4;
+const int64_t t = 4; //modulus
 const int64_t DELTA = q / t;
 
-// RNG
 random_device rd;
 mt19937 gen(rd());
 
-// ------------------------
-// Sampling
-// ------------------------
 
-Poly sample_ternary() {
+vector<int64_t> sample_ternary() {
     uniform_int_distribution<int> dist(-1, 1);
-    Poly res(N);
+    vector<int64_t> res(N);
     for (int i = 0; i < N; i++) res[i] = dist(gen);
     return res;
 }
 
-Poly sample_uniform() {
+vector<int64_t> sample_uniform() {
     uniform_int_distribution<int64_t> dist(0, q - 1);
-    Poly res(N);
+    vector<int64_t> res(N);
     for (int i = 0; i < N; i++) res[i] = dist(gen);
     return res;
 }
 
-Poly sample_noise() {
+vector<int64_t> sample_noise() {
     uniform_int_distribution<int> dist(-2, 2);
-    Poly res(N);
+    vector<int64_t> res(N);
     for (int i = 0; i < N; i++) res[i] = dist(gen);
     return res;
 }
 
-// ------------------------
-// Poly arithmetic
-// ------------------------
-
-Poly poly_add(const Poly &a, const Poly &b) {
-    Poly res(N);
+vector<int64_t> poly_add(const vector<int64_t> &a, const vector<int64_t> &b) {
+    vector<int64_t> res(N);
     for (int i = 0; i < N; i++) {
         res[i] = (a[i] + b[i]) % q;
         if (res[i] < 0) res[i] += q;
@@ -55,8 +44,8 @@ Poly poly_add(const Poly &a, const Poly &b) {
     return res;
 }
 
-Poly poly_scalar(const Poly &a, int64_t scalar) {
-    Poly res(N);
+vector<int64_t> poly_scalar(const vector<int64_t> &a, int64_t scalar) {
+    vector<int64_t> res(N);
     for (int i = 0; i < N; i++) {
         res[i] = (a[i] * scalar) % q;
         if (res[i] < 0) res[i] += q;
@@ -64,8 +53,8 @@ Poly poly_scalar(const Poly &a, int64_t scalar) {
     return res;
 }
 
-Poly poly_mul(const Poly &a, const Poly &b) {
-    Poly res(N, 0);
+vector<int64_t> poly_mul(const vector<int64_t> &a, const vector<int64_t> &b) {
+    vector<int64_t> res(N, 0);
 
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
@@ -84,27 +73,24 @@ Poly poly_mul(const Poly &a, const Poly &b) {
     return res;
 }
 
-// ------------------------
-// Keys
-// ------------------------
 
 struct PublicKey {
-    Poly b;
-    Poly a;
+    vector<int64_t> b;
+    vector<int64_t> a;
 };
 
 struct SecretKey {
-    Poly s;
+    vector<int64_t> s;
 };
 
 pair<PublicKey, SecretKey> keygen() {
-    Poly s = sample_ternary();
-    Poly a = sample_uniform();
-    Poly e = sample_noise();
+    vector<int64_t> s = sample_ternary();
+    vector<int64_t> a = sample_uniform();
+    vector<int64_t> e = sample_noise();
 
-    Poly a_s = poly_mul(a, s);
+    vector<int64_t> a_s = poly_mul(a, s);
 
-    Poly b(N);
+    vector<int64_t> b(N);
     for (int i = 0; i < N; i++) {
         b[i] = (-a_s[i] + e[i]) % q;
         if (b[i] < 0) b[i] += q;
@@ -113,42 +99,35 @@ pair<PublicKey, SecretKey> keygen() {
     return {{b, a}, {s}};
 }
 
-// ------------------------
-// Encryption
-// ------------------------
 
 struct Ciphertext {
-    Poly c0;
-    Poly c1;
+    vector<int64_t> c0;
+    vector<int64_t> c1;
 };
 
 Ciphertext encrypt(const PublicKey &pk, int m) {
-    Poly u = sample_ternary();
-    Poly e1 = sample_noise();
-    Poly e2 = sample_noise();
+    vector<int64_t> u = sample_ternary();
+    vector<int64_t> e1 = sample_noise();
+    vector<int64_t> e2 = sample_noise();
 
-    // Encode message
-    Poly m_poly(N, 0);
+    //encode message
+    vector<int64_t> m_poly(N, 0);
     m_poly[0] = (m % t + t) % t;
 
-    Poly scaled_m = poly_scalar(m_poly, DELTA);
+    vector<int64_t> scaled_m = poly_scalar(m_poly, DELTA);
 
-    Poly bu = poly_mul(pk.b, u);
-    Poly au = poly_mul(pk.a, u);
+    vector<int64_t> bu = poly_mul(pk.b, u);
+    vector<int64_t> au = poly_mul(pk.a, u);
 
-    Poly c0 = poly_add(poly_add(bu, e1), scaled_m);
-    Poly c1 = poly_add(au, e2);
+    vector<int64_t> c0 = poly_add(poly_add(bu, e1), scaled_m);
+    vector<int64_t> c1 = poly_add(au, e2);
 
     return {c0, c1};
 }
 
-// ------------------------
-// Decryption
-// ------------------------
-
 int decrypt(const Ciphertext &ct, const SecretKey &sk) {
-    Poly c1s = poly_mul(ct.c1, sk.s);
-    Poly x = poly_add(ct.c0, c1s);
+    vector<int64_t> c1s = poly_mul(ct.c1, sk.s);
+    vector<int64_t> x = poly_add(ct.c0, c1s);
 
     // Recover message from constant term
     double val = (double)x[0] / (double)DELTA;
@@ -158,19 +137,13 @@ int decrypt(const Ciphertext &ct, const SecretKey &sk) {
     return m;
 }
 
-// ------------------------
-// Debug
-// ------------------------
-
-void print_poly(const Poly &p, const string &name) {
+// Debug printing
+void print_poly(const vector<int64_t> &p, const string &name) {
     cout << name << ": [ ";
     for (auto v : p) cout << v << " ";
     cout << "]\n";
 }
 
-// ------------------------
-// Main test
-// ------------------------
 
 int main() {
     auto [pk, sk] = keygen();
