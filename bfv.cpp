7d2a2b0164d710e56186d1ee3a82ba0ed793807e
Matthew Bergin <mdb326@ariel.cse.lehigh.edu>
2026-05-02 12:04:45 -0400

    Add two ciphertexts together

--- bfv.cpp
+++ bfv.cpp
@@ -5,16 +5,16 @@
 
 using namespace std;
 
-const int N = 16;
-const int64_t q = 1 << 15; // 32768
-const int64_t t = 4; //modulus
+const int N = 16;  //must be power of 2
+const int64_t q = 1 << 18;
+const int64_t t = 64; //modulus, must be <<q
 const int64_t DELTA = q / t;
 
 random_device rd;
 mt19937 gen(rd());
 
 
-vector<int64_t> sample_ternary() {
+vector<int64_t> sample_ternary() { //-1,0,1
     uniform_int_distribution<int> dist(-1, 1);
     vector<int64_t> res(N);
     for (int i = 0; i < N; i++) res[i] = dist(gen);
@@ -73,7 +73,6 @@ vector<int64_t> poly_mul(const vector<int64_t> &a, const vector<int64_t> &b) {
     return res;
 }
 
-
 struct PublicKey {
     vector<int64_t> b;
     vector<int64_t> a;
@@ -104,6 +103,13 @@ struct Ciphertext {
     vector<int64_t> c0;
     vector<int64_t> c1;
 };
+
+Ciphertext add(const Ciphertext &a, const Ciphertext &b) {
+    Ciphertext res;
+    res.c0 = poly_add(a.c0, b.c0);
+    res.c1 = poly_add(a.c1, b.c1);
+    return res;
+}
 
 Ciphertext encrypt(const PublicKey &pk, int m) {
     vector<int64_t> u = sample_ternary();
@@ -144,17 +150,28 @@ void print_poly(const vector<int64_t> &p, const string &name) {
     cout << "]\n";
 }
 
+void print_ciphertext(const Ciphertext &ct) {
+    print_poly(ct.c0, "  c0");
+    print_poly(ct.c1, "  c1");
+}
+
 
 int main() {
     auto [pk, sk] = keygen();
 
-    int message = 3;
+    int message = 21;
+    int m2 = 22;
 
     cout << "Original message: " << message << endl;
 
     Ciphertext ct = encrypt(pk, message);
+    Ciphertext ct2 = encrypt(pk, m2);
 
-    int decrypted = decrypt(ct, sk);
+    Ciphertext res = add(ct, ct2);
+
+    print_ciphertext(res);
+
+    int decrypted = decrypt(res, sk);
 
     cout << "Decrypted message: " << decrypted << endl;
 
